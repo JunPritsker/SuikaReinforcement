92f3e026367686a20cdafbd91a83ae2dde784ca9
Jun Pritsker <junpritsker@gmail.com>
2026-04-18 20:42:45 -0400

    registration and configs for sb3_zoo training

--- rl_env/__init__.py
+++ rl_env/__init__.py
@@ -1,8 +1,15 @@
 from gymnasium.envs.registration import register
 
-# This registers your env so Gymnasium knows it exists
+# Register the environment so that rl_baseline3_zoo can find and train
+# with this environment
 register(
     id="SuikaEnv-v0",
-    # Path format: module_name.file_name:ClassName
     entry_point="rl_env.suika_env:SuikaEnv", 
+)
+
+# This env is registered so I can see how the training loop behaves 
+# so that I can tweak the code for better training
+register(
+    id="SuikaEnv-v0-test-setup",
+    entry_point="rl_env.suika_env:SuikaEnv",
 )
